14d1355b626ae1ccd706e4068b346fdb5c1ca0b2
alex121190 <aliaksei.lapkouski@gmail.com>
2020-02-12 09:36:29 -0500

    Задачка 1

--- HW_Algorithms_2/Задачка 1.py
+++ HW_Algorithms_2/Задачка 1.py
@@ -1,8 +1,21 @@
 from random import randint
 
-digit = int(input("Введите разрядность числа "))
+digits = int(input("Введите разрядность числа "))
 
-n = randint(0, 9)
 
-print(f"Мы получили число {n}")
+def sum_digits(digits):
+    if digits < 1:
+        print("Неверное значение, надо больше 1")
+    else:
+        down = 1 * 10 ** (digits - 1)
+        up = (1 * 10 ** digits) - 1
+        n = randint(down, up)
+        print(f"Мы получили число {n}")
+        result = 0
+        i = 0
+        while i < digits:
+            result += n % 10
+            n = n // 10
+            i += 1
 
+    return result
