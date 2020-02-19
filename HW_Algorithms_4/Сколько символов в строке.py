38d2dc0b17bfe8210504617f733487865b45c17f
alex121190 <aliaksei.lapkouski@gmail.com>
2020-02-19 00:17:32 -0500

    Задача по подсчету символов

--- HW_Algorithms_4/Сколько символов в строке.py
+++ HW_Algorithms_4/Сколько символов в строке.py
@@ -1,13 +1,21 @@
+s = input('Введите символ ')
+str1 = input('Введие строку ')
+
 def count_symbol(s, str1):
     sum_s = 0
     for i in str1:
         if i == s:
             sum_s += 1
     return 'Количество символов ' + s + ' в данной строке равно ' + str(sum_s)
+
+count_symbol(s, str1)
+print(count_symbol(s, str1))
+
+
 # Хочу  еще попробовать вывести другое сообщение когда нету такого символа, но пока не получается
 
         # else:
         #     return 'В данной строке нету таких символов'
 
 
-print(count_symbol('и', 'ивапи'))
+# print(count_symbol('и', 'ивап'))
