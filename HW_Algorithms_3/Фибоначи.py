b1e548f3902a7dabbefe9045ba4df61d75b0bde2
alex121190 <aliaksei.lapkouski@gmail.com>
2020-02-18 23:17:13 -0500

    Задание Фибоначи

--- HW_Algorithms_3/Фибоначи.py
+++ HW_Algorithms_3/Фибоначи.py
@@ -1,9 +1,23 @@
 
+n = int(input("Введите элемент ряда Фибоначи "))
+
 
 def fibonacci(n):
     fib1 = 1
-    fib2 = 1    if n == 0: hile i < n - 2:
-        fib_sum = fib1 + fib2
-        fib1, fib2 = fib2, fib_sum
-        i += 1
-        print(fib_sum)
+    if n < 0:
+        print(f'Неверное значение')
+        quit()
+    elif n == 0:
+        print(0)
+    elif n == 1 or n == 2:
+        print(fib1)
+    else:
+        fib2 = 1
+        i = 0
+        while i < n - 2:
+            fib1, fib2 = fib2, fib1 + fib2
+            i = i + 1
+            if i == n - 2:
+                print(fib2)
+
+fibonacci(n)
